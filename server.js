12f85fdec4e0e3dc4fe6f839ea652fcc029ba6e3
Guilherme Sousa <guilherme.tec1107@gmail.com>
2022-06-12 16:06:37 -0300

    server skeleton

--- server.js
+++ server.js
@@ -1,5 +1,49 @@
-(async () => {
+const express = require("express")
+const routes = require("./src/routes/routes.js")
+const database = require('./src/db')
+
+require("dotenv").config()
+
+
+
+(async () => { await database.sync({ force: true }) })
+
+const app = express()
+
+app.use("/", routes)
+
+
+/* (async () => {
     const database = require('./src/db')
-    const Client = require('./src/tables/client')
-    await database.sync()
-})() 
+    const Client = await require('./src/tables/client')
+    const Product = require('./src/tables/product')
+    const Request = require('./src/tables/request')
+    const Category = require('./src/tables/category')
+    await database.sync({force: true})
+
+    const newClient = await Client.create({
+        name: "paa",
+        email:"kdk",
+        password:'dddd'
+    })
+
+    const newProduct = await Product.create({
+        id: "asdsd",
+        title: "sdfsdf",
+        price: 22,
+        available_quantity: 12
+    })
+
+    const newRequest = await Request.create({
+        client_id: newClient.id,
+        product_id: newProduct.id
+    })
+
+    const newCategory = await Category.create({
+        id: "asd",
+        name: "dfsdfsdfsdfsd"
+    })
+
+    await newClient.save()
+    await newRequest.save()
+})()  */
