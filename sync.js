dcd0f7f95bb26e9c6c5717c97c25b2c8cd47673d
Guilherme Sousa <guilherme.tec1107@gmail.com>
2022-06-14 17:29:32 -0300

    Code database sync code, done

---
+++ sync.js
@@ -0,0 +1,67 @@
+const product = require('./src/models/product')
+const category = require('./src/models/category')
+const axios = require('axios')
+
+function syncDB() {
+
+    axios.get('https://api.mercadolibre.com/sites/MLB/categories').then(async (resApi) => {
+
+        const categories = resApi.data
+        const categoriesFiltred = []
+
+
+        categories.forEach(e => {
+            if (e.id === process.env.CATEGORY1 || e.id === process.env.CATEGORY2 || e.id === process.env.CATEGORY3 || e.id === process.env.CATEGORY4) {
+                categoriesFiltred.push(e)
+            }
+        })
+
+        for (let i = 0; i < categoriesFiltred.length; i++) {
+
+            const e = categoriesFiltred[i]
+
+            try {
+                const categoryModel = await category.create({
+                    id: e.id,
+                    name: e.name
+                })
+                categoryModel.save()
+            } catch (err) {
+                console.log(err.message)
+            }
+
+        }
+
+        for (let i = 0; i < categoriesFiltred.length; i++) {
+            const e = categoriesFiltred[i]
+
+            axios.get(`https://api.mercadolibre.com/sites/MLB/search?category=${e.id}`).then(async (resApi) => {
+                for (let j = 0; j < 4; j++) {
+
+
+                    const productApi = resApi.data.results[j]
+
+
+                    try {
+                        const productModel = await product.create({
+                            id: productApi.id,
+                            title: productApi.title,
+                            price: productApi.price,
+                            available_quantity: productApi.available_quantity
+                        })
+                        await productModel.save()
+
+                    } catch (err) {
+
+                        console.log(err.message)
+                    }
+
+                }
+            })
+        }
+
+    })
+
+}
+
+syncDB()
